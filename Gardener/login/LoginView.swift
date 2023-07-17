afd2e4bcb24e3a4bb85733d148af9244b393cffa
ryuhyeaonJin <dbguswls030@gmail.com>
2023-07-17 17:52:51 +0900

    [feat] loginView 전화번호 인증으로 수정

--- Gardener/login/LoginView.swift
+++ Gardener/login/LoginView.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import GoogleSignIn
 import SnapKit
 
 class LoginView: UIView{
@@ -15,10 +14,29 @@ class LoginView: UIView{
         return UIImageView(image: UIImage(named: "ralo"))
     }()
     
-    private lazy var googleLoginButton: GIDSignInButton = {
-        var button = GIDSignInButton()
-        button.style = .wide
-//        button.contentHorizontalAlignment = .center
+    private lazy var phoneNumberTextField: UITextField = {
+        var textField = UITextField()
+        textField.placeholder = "휴대폰 번호(- 없이 숫자만 입력)"
+        textField.layer.borderColor = UIColor.darkGray.cgColor
+        textField.layer.borderWidth = 1.1
+        textField.layer.cornerRadius = 5
+        textField.leftView = UIView(frame: .init(x: 0, y: 0, width: 13, height: 0))
+        textField.leftViewMode = .always
+        textField.keyboardType = .numberPad
+        textField.delegate = self
+        return textField
+    }()
+    
+    private lazy var submitButton: UIButton = {
+        var button = UIButton()
+        button.layer.borderColor = UIColor.darkGray.cgColor
+        button.layer.borderWidth = 1.1
+        button.layer.cornerRadius = 5
+        button.isEnabled = false
+        button.backgroundColor = .lightGray
+        button.setTitle("인증하기", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        // TODO: 텍스트 Bold
         return button
     }()
     
@@ -34,24 +52,54 @@ class LoginView: UIView{
     func initUI(){
         backgroundColor = .white
         self.addSubview(tempImage)
-        self.addSubview(googleLoginButton)
+        self.addSubview(phoneNumberTextField)
+        self.addSubview(submitButton)
         
         tempImage.snp.makeConstraints { make in
-            make.top.equalTo(100)
+            make.top.equalTo(80)
             make.width.equalTo(230)
             make.height.equalTo(230)
             make.centerX.equalToSuperview()
         }
         
-        googleLoginButton.snp.makeConstraints { make in
-            make.top.equalTo(tempImage.snp.bottom).offset(50)
-            make.centerX.equalToSuperview()
-            make.left.equalToSuperview().offset(50)
-            make.right.equalToSuperview().offset(-50)
+        phoneNumberTextField.snp.makeConstraints { make in
+            make.top.equalTo(tempImage.snp.bottom).offset(30)
+            make.left.equalToSuperview().offset(30)
+            make.right.equalToSuperview().offset(-30)
+            make.height.equalTo(45)
+        }
+        
+        submitButton.snp.makeConstraints { make in
+            make.top.equalTo(phoneNumberTextField.snp.bottom).offset(20)
+            make.left.equalToSuperview().offset(30)
+            make.right.equalToSuperview().offset(-30)
+            make.height.equalTo(45)
+        }
+        phoneNumberTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+    }
+}
+extension LoginView: UITextFieldDelegate{
+    @objc func textFieldDidChange(_ sender: Any?){
+        if let text = self.phoneNumberTextField.text{
+            if text.count < 11{
+                submitButton.isEnabled = false
+                submitButton.backgroundColor = .lightGray
+            }else{
+                submitButton.isEnabled = true
+                submitButton.backgroundColor = .green
+            }
         }
     }
-    
-    func googleLoginButtonAction(target: UIViewController, action: Selector){
-        googleLoginButton.addTarget(target, action: action, for: .touchUpInside)
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if let char = string.cString(using: String.Encoding.utf8) {
+            let isBackSpace = strcmp(char, "\\b")
+            if isBackSpace == -92 {
+                return true
+            }
+        }
+        guard textField.text!.count < 11 else{
+            return false
+        }
+        return true
     }
 }
--- Gardener/login/LoginViewController.swift
+++ Gardener/login/LoginViewController.swift
@@ -9,21 +9,18 @@ import UIKit
 import SnapKit
 import FirebaseCore
 import FirebaseAuth
-import GoogleSignIn
 
 class LoginViewController: UIViewController {
     
     private lazy var loginView: LoginView = {
         let view = LoginView()
-        view.googleLoginButtonAction(target: self, action: #selector(googleLogin))
-       return view
+        return view
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         initUI()
-        
     }
     
     private func initUI(){
@@ -34,51 +31,13 @@ class LoginViewController: UIViewController {
         loginView.initUI()
     }
     
-    @objc private func googleLogin(){
-        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
-        
-        // Create Google Sign In configuration object.
-        let config = GIDConfiguration(clientID: clientID)
-        GIDSignIn.sharedInstance.configuration = config
-        
-        // Start the sign in flow!
-        GIDSignIn.sharedInstance.signIn(withPresenting: self) { [unowned self] result, error in
-            guard error == nil else {
-                return
-            }
-            guard let user = result?.user, let idToken = user.idToken?.tokenString
-            else {
-                return
-            }
-            let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: user.accessToken.tokenString)
-            
-            Auth.auth().signIn(with: credential) { result, error in
-                // MARK: TODO 로그인 실패 시 처리
-                guard error == nil else{
-                    print("Firebase SignIn failed")
-                    return
-                }
-                let vc = MainTabBarController()
-                vc.modalPresentationStyle = .fullScreen
-                self.present(vc, animated: true)
-            }
-        }
-    }
     private func logout(){
-        // 구글 로그아웃
-        GIDSignIn.sharedInstance.signOut()
-        
         // 파이어베이스 로그아웃
         let firebaseAuth = Auth.auth()
         do {
-          try firebaseAuth.signOut()
+            try firebaseAuth.signOut()
         } catch let signOutError as NSError {
-          print("Error signing out: %@", signOutError)
-        }
-        
-        // 구글 로그인 연결해제
-        GIDSignIn.sharedInstance.disconnect { error in
-            guard error == nil else { return }
+            print("Error signing out: %@", signOutError)
         }
     }
 }
